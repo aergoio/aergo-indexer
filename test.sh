527d1c121ac96f784d02eae15040f1904f6b1c57
Paul Grau <paul@blocko.io>
2019-11-01 17:12:13 +0900

    Remove unused code

--- test.sh
+++ test.sh
@@ -11,24 +11,26 @@ echo "Starting mariadb"
 docker run -d -p 3306:3306 --name mariadb_test -e MYSQL_ROOT_PASSWORD=my-secret-pw mariadb:10.4-bionic
 
 echo "Starting aergosvr"
-docker run -d -p 7845:7845 --name aergo_test aergo/node:1.2 aergosvr --config /aergo/testmode.toml
+docker run -d -p 7845:7845 --name aergo_test aergo/node:1.3 aergosvr --config /aergo/testmode.toml
+#docker run -d -p 7845:7845 --name aergo_test aergo/node:2.0.0-rc1 aergosvr --config /aergo/testmode.toml
 
 #docker run -d -p 7845:7845 --name aergo_test aergo/node
 echo "Starting indexer"
 sleep 3
 
 #AERGO_URL="localhost:7845"
-AERGO_URL="testnet.aergo.io:7845"
+AERGO_URL="testnet-api.aergo.io:7845"
 ES_URL="http://localhost:9200"
 MARIADB_URL="root:my-secret-pw@tcp(localhost:3306)/test"
 CHAIN_PREFIX="chain_"
 SYNC_FROM=0
 SYNC_TO=9999
 
-#time ./bin/indexer -A $AERGO_URL --dbtype mariadb --dburl $MARIADB_URL --prefix $CHAIN_PREFIX --from $SYNC_FROM --to $SYNC_TO --exit-on-complete --reindex 
-#time ./bin/indexer -A $AERGO_URL --dbtype elastic --dburl $ES_URL --prefix $CHAIN_PREFIX --from $SYNC_FROM --to $SYNC_TO --exit-on-complete --reindex 
+time ./bin/indexer -A $AERGO_URL --dbtype mariadb --dburl $MARIADB_URL --prefix $CHAIN_PREFIX --from $SYNC_FROM --to $SYNC_TO --exit-on-complete --reindex 
+time ./bin/indexer -A $AERGO_URL --dbtype elastic --dburl $ES_URL --prefix $CHAIN_PREFIX --from $SYNC_FROM --to $SYNC_TO --exit-on-complete --reindex 
+# time ../aergo-esindexer/bin/esindexer -A $AERGO_URL -E $ES_URL --prefix old_$CHAIN_PREFIX --exit-on-complete --reindex 
 
-./bin/indexer -H localhost -p 7845 --dbtype mariadb --dburl "root:my-secret-pw@tcp(localhost:3306)/test" --prefix chain_
+#./bin/indexer -H localhost -p 7845 --dbtype mariadb --dburl "root:my-secret-pw@tcp(localhost:3306)/test" --prefix chain_
 #./bin/indexer -H localhost -p 7845 --dburl http://localhost:9200 --prefix chain_
 # ./bin/indexer -H localhost -p 7845 --dburl http://localhost:9200 --prefix chain_ --reindex
 #./bin/indexer -H localhost -p 7845 --dburl http://localhost:9200 -A testnet.aergo.io:7845 --reindex --prefix chain_
